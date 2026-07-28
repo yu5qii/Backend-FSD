126657cb658b63187fb0d0b4711f82a40844de57
Ridah <ridah.25b01010047@abes.ac.in>
2026-07-28 09:42:26 +0530

    await

--- 2807.js
+++ 2807.js
@@ -1,20 +1,29 @@
 //PROMISES FOR ASYNCHRONOUS
-const promiseOne = new Promise((resolve, reject) => {
-    console.log("Promise task 1");
-    resolve("promises passed using resolve");
-    let msg = false;
-    if (!msg) {
-        console.log("message using promises failed");
-    }
-    else {
-        console.log("--------error-------");
-    }
-    setTimeout(() => {      //setTimeout() is also asynch
-        console.log("waited for 2 sec");
-    }, 2000)
-});
-promiseOne.then((result) => {
-    console.log("result");
-}).catch((error) => {
-    console.log(error);
-})
+// new Promise((resolve, reject) => {      //can also store this in a variable
+//     console.log("Promise task 1");
+//     resolve("promises passed using resolve");
+//     let msg = false;
+//     if (!msg) {
+//         console.log("message using promises failed");
+//     }
+//     else {
+//         console.log("--------error-------");
+//     }
+//     setTimeout(() => {      //setTimeout() is also asynch
+//         console.log("waited for 2 sec");
+//     }, 2000)
+// }).then((result) => {
+//     console.log("result");
+// }).catch((error) => {
+//     console.log(error);
+// })
+
+//ASYNCH AWAIT
+console.log("1")
+async function test() {
+    console.log("2");
+    await console.log("3");
+    console.log("4");
+    console.log("5");
+}
+test()
