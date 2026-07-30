badc995d4c6ac93d548bf4cc2b8d19c11211277a
Ridah <ridah.25b01010047@abes.ac.in>
2026-07-30 11:10:57 +0530

    fetch and catch

--- 2807.js
+++ 2807.js
@@ -19,43 +19,57 @@
 // })
 
 //ASYNCH AWAIT
-console.log("1")
-async function test() {
-    console.log("2");
-    await console.log("3");
-    console.log("4");
-    console.log("5");
-}
-test();
+// console.log("1")
+// async function test() {
+//     console.log("2");
+//     await console.log("3");
+//     console.log("4");
+//     console.log("5");
+// }
+// test();
 
 //create promises that will print username and password using ___
 //and if username and password are not found then it will call call
 //reject state and print error
 
-const checkCredentials = (username, password) => {
-  return new Promise((resolve, reject) => {
-    const isFound = (username && password) ? true : false;
+// const checkCredentials = (username, password) => {
+//   return new Promise((resolve, reject) => {
+//     const isFound = (username && password) ? true : false;
 
-    if (isFound) {
-      resolve({ user: username, pass: password });
-    } else {
-      reject(new Error("Error: Username and password not found."));
-    }
-  });
-};
+//     if (isFound) {
+//       resolve({ user: username, pass: password });
+//     } else {
+//       reject(new Error("Error: Username and password not found."));
+//     }
+//   });
+// };
 
-checkCredentials("Ridah", "123123")
-  .then((data) => {
-    console.log(`Username: ${data.user}, Password: ${data.pass}`);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+// checkCredentials("Ridah", "123123")
+//   .then((data) => {
+//     console.log(`Username: ${data.user}, Password: ${data.pass}`);
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
 
-checkCredentials("", null)
-  .then((data) => {
-    console.log(`Username: ${data.user}, Password: ${data.pass}`);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+// checkCredentials("", null)
+//   .then((data) => {
+//     console.log(`Username: ${data.user}, Password: ${data.pass}`);
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
+
+async function test(){
+    console.log("message: 1");
+    const response = await fetch("./student_data.json");
+    console.log(response.status)
+    const std = await response.json();
+    return std;
+    console.log("message: 3");
+}
+test().then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.error("error: ", error);
+});
