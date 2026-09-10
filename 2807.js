10c6a43eabda1dff45b4b3ff93b607ab3c92fe50
Ridah <ridah.25b01010047@abes.ac.in>
2026-09-10 11:01:55 +0530

    updating files

--- 2807.js
+++ 2807.js
@@ -1,22 +1,22 @@
 //PROMISES FOR ASYNCHRONOUS
-// new Promise((resolve, reject) => {      //can also store this in a variable
-//     console.log("Promise task 1");
-//     resolve("promises passed using resolve");
-//     let msg = false;
-//     if (!msg) {
-//         console.log("message using promises failed");
-//     }
-//     else {
-//         console.log("--------error-------");
-//     }
-//     setTimeout(() => {      //setTimeout() is also asynch
-//         console.log("waited for 2 sec");
-//     }, 2000)
-// }).then((result) => {
-//     console.log("result");
-// }).catch((error) => {
-//     console.log(error);
-// })
+new Promise((resolve, reject) => {      //can also store this in a variable
+    console.log("Promise task 1");
+    resolve("promises passed using resolve");
+    let msg = false;
+    if (!msg) {
+        console.log("message using promises failed");
+    }
+    else {
+        console.log("--------error-------");
+    }
+    setTimeout(() => {      //setTimeout() is also asynch
+        console.log("waited for 2 sec");
+    }, 2000)
+}).then((result) => {
+    console.log("result");
+}).catch((error) => {
+    console.log(error);
+})
 
 //ASYNCH AWAIT
 // console.log("1")
