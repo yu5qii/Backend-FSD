dc6cacd626685da3c6b608ffe6814ff5759d8521
Ridah <ridah.25b01010047@abes.ac.in>
2026-09-17 10:56:49 +0530

    product rest api

--- expressReact/index.js
+++ expressReact/index.js
@@ -1,20 +1,58 @@
-import express from'express';
-import cors from 'cors';
-import fs from 'fs';
-// import product from './product.json' with { type: 'json' };
+import express from "express";
+import fs from "fs";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-//GET
-app.get('/products', (req, res) => {
-    const data = fs.readFile("product.json", "utf-8");
-    const products = JSON.parse(data);
-    res.json(products);
-})
+// GET products
+app.get("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
 
-app.listen(4000, () => {
-    console.log("app running on port: https://localhost: 4000");
-})
+  const products = JSON.parse(data);
+
+  res.json(products);
+});
+
+// POST product
+app.post("/api/products", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
+
+  const products = JSON.parse(data);
+
+  const newProduct = {
+    id: products.length + 1,
+    name: req.body.name,
+    price: req.body.price,
+    category: req.body.category,
+  };
+
+  products.push(newProduct);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json(newProduct);
+});
+
+// DELETE product
+app.delete("/api/products/:id", (req, res) => {
+  const data = fs.readFileSync("products.json", "utf-8");
+
+  let products = JSON.parse(data);
+
+  const id = parseInt(req.params.id);
+
+  products = products.filter((product) => product.id !== id);
+
+  fs.writeFileSync("products.json", JSON.stringify(products, null, 2));
+
+  res.json({
+    message: "Product deleted successfully",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
